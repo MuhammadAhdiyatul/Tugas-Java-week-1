6628f2b9000e3dd5b33ef800950185c4ef5bf3dc
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-02-14 19:16:37 +0700

    jawaban nomor 2

---
+++ Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static void main(String[] aargs) {
+        final double CM_PER_INCH = 2.54; double paperWidth  = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters : " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+    }
+}
